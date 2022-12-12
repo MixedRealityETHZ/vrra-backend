2e373e4878aee0856ce60e42229643c032aa7608
Haoyang Zhou <me@howyoung.dev>
2022-12-12 17:43:14 +0100

    feat: add with SSL option to Minio

--- RoomArrangementsBackend/Program.cs
+++ RoomArrangementsBackend/Program.cs
@@ -6,10 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddDbContext<DataContext>(options =>
-  options.UseNpgsql(builder.Configuration.GetConnectionString("DataContext")));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DataContext")));
 
 var minioConfig = builder.Configuration.GetSection("Minio");
-builder.Services.AddMinio(new Uri(minioConfig["Uri"]));
+builder.Services.AddMinio(new Uri(minioConfig["Uri"]), option =>
+{
+    option.ConfigureClient(client =>
+    {
+        if (minioConfig["WithSSL"] == "True")
+        {
+            client.WithSSL();
+        }
+    });
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
